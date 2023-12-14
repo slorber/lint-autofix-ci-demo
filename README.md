3a0bbde13c9e81d44657b59c0939605382fae705
sebastienlorber <lorber.sebastien@gmail.com>
2023-12-14 14:12:39 +0100

    Add initial project code

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# Lint Autofix CI Demo
+
+A demo of a GitHub Actions setup that automatically fixes PRs of external contributors.
+
---
+++ index.js
@@ -0,0 +1,37 @@
+// Default code sample from the Prettier Playground: https://prettier.io/playground/
+
+
+
+function HelloWorld({
+  greeting = "hello",
+  greeted = '"World"',
+  silent = false,
+  onMouseOver,
+}) {
+  if (!greeting) {
+    return null;
+  }
+
+  let num = Math.floor(Math.random() * 1e7)
+    .toString()
+    .replace(/\.\d+/gi, "");
+
+  return (
+    <div
+      className="HelloWorld"
+      title={`You are visitor number ${num}`}
+      onMouseOver={onMouseOver}
+    >
+      <strong>
+        {greeting.slice(0, 1).toUpperCase() + greeting.slice(1).toLowerCase()}
+      </strong>
+      {greeting.endsWith(",") ? (
+        " "
+      ) : (
+        <span style={{ color: "grey" }}>", "</span>
+      )}
+      <em>{greeted}</em>
+      {silent ? "." : "!"}
+    </div>
+  );
+}
